b8d0a8f2b48960ab6832b05419b6ee5be1458357
Joao Simoes <44359911+simoespt@users.noreply.github.com>
2025-11-22 21:30:17 +0000

    Create Input component with forwardRef

---
+++ componentes/ui/input.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`flex h-10 w-full rounded-xl border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black/20 disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
